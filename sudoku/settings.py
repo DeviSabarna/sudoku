c3c51a100516d44de94500bd1e980c92f2da724b
Devi <devs2@cisco.com>
2021-04-16 09:08:42 +0530

    Added Host

--- sudoku/settings.py
+++ sudoku/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'w1kwa&g^=9g!8xwof-&*0uycdc3+=l$_z0m5nzozg(3cgb__pu'
-
+#SECRET_KEY = 'w1kwa&g^=9g!8xwof-&*0uycdc3+=l$_z0m5nzozg(3cgb__pu'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mysudokuapp.herokuapp.com']
 
 
 # Application definition
